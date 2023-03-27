5bb06a09f2bc24d78958f3517f864adf87c490b5
Vampo <umangboss786@gmail.com>
2023-03-27 23:54:23 +0530

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+### Gasless POAPs Creator
+
+Create minting QR Codes for gasless POAPs/NFTs on the Solana blockchain! follow these simple steps to get started,
+
+Note: This is an open-sourced, example implementation, security of private keys being used to pay for gas behind the scenes is at your risk!
+
+## Fork & Edit
+
+Fork the repo and edit the following details based on your NFT details,
+ - Custom RPC URL under `./nft.ts` for better speed (optional)
+ - `uri` under the `./nft.ts` file. This holds the metadata details of your NFT. Create a new JSON file with required details, host on pltaforms like Shadow.storage or Cloudinary and paste the hosted url here to mint NFT with your required metadata (required)
+
+## [![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/template/ClTh95?referralCode=vamp) 
+Deploy on Railway and make sure to add value to the env `SOL_PRIVATE_KEY` with private key of wallet that will pay for gas fees behind the scenes! (Each NFT mint costs ~0.012 SOL, fund according to your expected mint numbers!)
+
+## Encode URL of the hosted server
+
+ - After deployment on railway, your server will be hosted! Create a domain for your server under `custom domain` settings and copy the URL.
+ - Visit [URL Encoder](https://www.urlencoder.org/), psste your server URL, click encode, an encoded URL will be generated. Copy it for the final step
+
+## Create QR Code
+
+ - Visit [QR Code Monkey](https://www.qrcode-monkey.com/#), under your URL section, add a text `solana:` in start and then paste your encoded server url. Note- Adding the `solana:` text is reuired and minting will not work if this process is not followed properly!
+ ![qr-code-monkey](https://shdw-drive.genesysgo.net/BfBZRXtX2ad9dVyJnc6Tbww8egupegtiV2xiwWCBYH1h/Screenshot%202023-03-27%20234846.png)
+ - Design your QR Code according to your needs and download the final image.
+
+ That's all! you're all set to mint and share some gasless NFTs on the Solana blockchain!
---
+++ src/nft.ts
@@ -0,0 +1,55 @@
+import * as anchor from "@project-serum/anchor";
+import { Metaplex, keypairIdentity } from "@metaplex-foundation/js";
+import base58 from "bs58";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const mint = async (walletAddress: string) => {
+    const connection = new anchor.web3.Connection(
+        "https://solana-mainnet.rpc.extrnode.com"  // replace with your RPC
+    );
+    const user = new anchor.web3.PublicKey(walletAddress);
+    const reference = anchor.web3.Keypair.generate().publicKey;
+    const mint = anchor.web3.Keypair.generate();
+    const keypair = anchor.web3.Keypair.fromSecretKey(
+        base58.decode(process.env.SOL_PRIVATE_KEY!) // add your payer private key, this wallet is the actual creator of NFT
+    );
+    const metaplex = Metaplex.make(connection).use(keypairIdentity(keypair));
+
+    const transactionBuilder = await metaplex.nfts().builders().create({
+        uri: "https://res.cloudinary.com/dtzqgftjk/raw/upload/v1678791227/vietnam-hh-metadata_ib5t3e.json", // replace with with solana NFT standard compatible JSON file
+        name: "Solana HH Attendance Token", //name of your NFT
+        symbol: "HH", //symbol of your NFT
+        sellerFeeBasisPoints: 0,
+        useNewMint: mint,
+        tokenOwner: user,
+    });
+
+    const { blockhash, lastValidBlockHeight } =
+        await connection.getLatestBlockhash();
+    const transaction = transactionBuilder.toTransaction({
+        blockhash,
+        lastValidBlockHeight,
+    });
+
+    transaction.instructions[0].keys.push({
+        pubkey: user,
+        isSigner: true,
+        isWritable: false,
+    });
+    transaction.instructions[0].keys.push({
+        pubkey: reference,
+        isSigner: false,
+        isWritable: false,
+    });
+
+    transaction.partialSign(mint, keypair);
+
+    const serializedTransaction = transaction.serialize({
+        requireAllSignatures: false,
+    });
+    const base64 = serializedTransaction.toString("base64");
+
+    return base64;
+};
