557573dec94124ba64a4ae9515a47f529b276f87
Kushal Jain <jain.kushal10@gmail.com>
2023-06-03 20:20:58 +0530

    removed app.get()

--- src/index.ts
+++ src/index.ts
@@ -26,58 +26,57 @@ type ErrorOutput = {
 
 const app = express();
 
-app.get('/:poap', (req: Request) => {
-  let poap = req.params.poap;
-  app.all(
-    "/",
-    async (
+app.all(
+  "/:poap",
+  async (
+    req: Request,
+    res: Response<
+      MakeTransactionGetResponse | MakeTransactionOutputData | ErrorOutput
+    >
+  ) => {
+    let poap = req.params.poap;
+    
+    function get(res: Response<MakeTransactionGetResponse>) {
+      res.status(200).json({
+        label: "Zo World POAPs", 
+        icon: "https://res.cloudinary.com/dnjbui12k/image/upload/v1685694169/Zo_Orange_Logo_hodhh6.png",
+      });
+    }
+
+    async function post(
       req: Request,
-      res: Response<
-        MakeTransactionGetResponse | MakeTransactionOutputData | ErrorOutput
-      >
-    ) => {
-      function get(res: Response<MakeTransactionGetResponse>) {
-        res.status(200).json({
-          label: "Zo World POAPs", 
-          icon: "https://res.cloudinary.com/dnjbui12k/image/upload/v1685694169/Zo_Orange_Logo_hodhh6.png",
-        });
-      }
-  
-      async function post(
-        req: Request,
-        res: Response<MakeTransactionOutputData | ErrorOutput>
-      ) {
-        try {
-          const { account } = req.body as MakeTransactionInputData;
-          if (!account) {
-            res.status(40).json({ error: "No account provided" });
-            return;
-          }
-          console.log(account as string);
-  
-          const base64 = await mint(account, poap as PoapKeys);
-  
-          return res.status(200).json({
-            transaction: base64,
-            message: "powered by Zo World",
-          });
-        } catch (err) {
-          console.error(err);
-          res.status(500).json({ error: "error creating transaction" });
+      res: Response<MakeTransactionOutputData | ErrorOutput>
+    ) {
+      try {
+        const { account } = req.body as MakeTransactionInputData;
+        if (!account) {
+          res.status(40).json({ error: "No account provided" });
           return;
         }
-      }
-  
-      if (req.method === "GET") {
-        return get(res);
-      } else if (req.method === "POST") {
-        return await post(req, res);
-      } else {
-        return res.status(405).json({ error: "Method not allowed" });
+        console.log(account as string);
+
+        const base64 = await mint(account, poap as PoapKeys);
+
+        return res.status(200).json({
+          transaction: base64,
+          message: "powered by Zo World",
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "error creating transaction" });
+        return;
       }
     }
-  );
-});
+
+    if (req.method === "GET") {
+      return get(res);
+    } else if (req.method === "POST") {
+      return await post(req, res);
+    } else {
+      return res.status(405).json({ error: "Method not allowed" });
+    }
+  }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
