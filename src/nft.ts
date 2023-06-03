10ac614adf722cab01d3ce57b6a431c5ca09617b
Kushal Jain <jain.kushal10@gmail.com>
2023-06-04 04:26:50 +0530

    added all events for poaps in PoapKeys

--- src/nft.ts
+++ src/nft.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export type PoapKeys = 'yoga' | 'crossfit';
+export type PoapKeys = 'yoga' | 'crossfit' | 'sundowner' | 'pool-party' | 'fifa' | 'hiking' | 'open-mic' | 'mortal-kombat' | 'binge-watch' | 'deep-work' | 'card-game' | 'chai-pe-charcha' | 'tech-talks';
 
 interface Poap {
     uri: string;
@@ -20,9 +20,64 @@ interface Poap {
       'symbol': "YOGA"
     },
     'crossfit': {
-        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685797736/yoga_sdqjqm.json",
-        'name': "Yoga @ BLRxZo",
-        'symbol': "YOGA"
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832503/crossfit_zlr67i.json",
+        'name': "CrossFit @ BLRxZo",
+        'symbol': "CF"
+      },
+      'sundowner': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832504/sundowner_kd5yke.json",
+        'name': "Sundowner @ BLRxZo",
+        'symbol': "SUNDOWNER"
+      },
+      'pool-party': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832504/pool-party_xqciek.json",
+        'name': "Pool Party @ BLRxZo",
+        'symbol': "POOL"
+      },
+      'fifa': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832504/fifa_fjnfqe.json",
+        'name': "FIFA @ BLRxZo",
+        'symbol': "FIFA"
+      },
+      'hiking': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832504/hiking_sindto.json",
+        'name': "Hiking @ BLRxZo",
+        'symbol': "HIKING"
+      },
+      'open-mic': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832504/open-mic_tqhemf.json",
+        'name': "Open Mic @ BLRxZo",
+        'symbol': "OPENMIC"
+      },
+      'mortal-kombat': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832504/mortal-combat_v7bsxw.json",
+        'name': "Mortal Kombat @ BLRxZo",
+        'symbol': "MORTAL"
+      },
+      'binge-watch': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832504/binge-watch_mfxw2f.json",
+        'name': "Binge Watch @ BLRxZo",
+        'symbol': "BINGE"
+      },
+      'deep-work': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832503/deep-work_zsb6qy.json",
+        'name': "Deep Work @ BLRxZo",
+        'symbol': "DW"
+      },
+      'chai-pe-charcha': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832503/chai-pe-charcha_arbtzd.json",
+        'name': "Deep Work @ BLRxZo",
+        'symbol': "DW"
+      },
+      'card-game': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832503/card-game_yqzkev.json",
+        'name': "Card Games @ BLRxZo",
+        'symbol': "CG"
+      },
+      'tech-talks': {
+        'uri': "https://res.cloudinary.com/dnjbui12k/raw/upload/v1685832503/deep-work_zsb6qy.json",
+        'name': "Tech Talk @ BLRxZo",
+        'symbol': "TECHTALK"
       }
   }
 
